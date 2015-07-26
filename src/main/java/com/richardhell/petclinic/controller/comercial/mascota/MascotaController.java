dce46dd50887867b9e7e3e252bb87bdf990a17b8
Jose Eusebio Alderete <jose@Joses-MacBook-Air.local>
2015-07-26 09:11:58 -0500

    fixs

--- src/main/java/com/richardhell/petclinic/controller/comercial/mascota/MascotaController.java
+++ src/main/java/com/richardhell/petclinic/controller/comercial/mascota/MascotaController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("com/especie")
+@RequestMapping("com/mascota")
 public class MascotaController {
 
 }
